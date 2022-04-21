299e0a56a23b14fdc2ccc8df610a0fb3625a9984
khoaguin <dkn.work@protonmail.com>
2022-04-21 15:01:17 +0700

    first commit

---
+++ course1-algorithmic-toolbox/week1_programming_challenges/1_sum_of_two_digits/APlusB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int sum_of_two_digits(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int x = 0;
+    int y = 0;
+    std::cin >> x;
+    std::cin >> y;
+    std::cout << sum_of_two_digits(x, y);
+    return 0;
+}
